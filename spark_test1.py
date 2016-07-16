417984b0b9165d0759533ed4da8ce165395c19b5
leo <luohuayong@163.com>
2016-07-15 23:15:17 -0700

    first spark demo

---
+++ spark_test1.py
@@ -0,0 +1,17 @@
+from  pyspark.mllib.recommendation import ALS,Rating
+from pyspark import  SparkContext
+
+sc = SparkContext("local[2]","first spark app")
+rawdata = sc.textFile("./ml-100k/u.data")
+print rawdata.first()
+data = rawdata.map(lambda x:x.split('\t'))
+print data.take(5)
+ratings = data.map(lambda x:Rating(int(x[0]),int(x[1]),float(x[2])))
+print ratings.first()
+model = ALS.train(ratings,50)
+features = model.userFeatures()
+print features.take(2)
+predict = model.predict(789,123)
+print predict
+
+
