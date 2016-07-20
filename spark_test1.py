9da2785f0062ed27bba743640289dd76d3cef0ff
leo <luohuayong@163.com>
2016-07-20 03:14:08 -0700

    spark create model

--- spark_test1.py
+++ spark_test1.py
@@ -3,15 +3,17 @@ from pyspark import  SparkContext
 
 sc = SparkContext("local[2]","first spark app")
 rawdata = sc.textFile("./ml-100k/u.data")
-print rawdata.first()
+#print rawdata.first()
 data = rawdata.map(lambda x:x.split('\t'))
-print data.take(5)
+#print data.take(5)
 ratings = data.map(lambda x:Rating(int(x[0]),int(x[1]),float(x[2])))
-print ratings.first()
+#print ratings.first()
 model = ALS.train(ratings,50)
 features = model.userFeatures()
-print features.take(2)
+#print features.take(2)
 predict = model.predict(789,123)
 print predict
+top = model.recommendProducts(789,10)
+print top
 
 
