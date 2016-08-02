eff1762b512dc2757bc265e4b96c72dba9592ae8
leo <luohuayong@163.com>
2016-08-01 18:51:04 -0700

    del

--- Recommend_test/hopin_load_model.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext
-from pyspark.mllib.recommendation import MatrixFactorizationModel
-model_path = "hopin_model"
-sc = SparkContext("local[2]","load_model")
-model = MatrixFactorizationModel.load(sc,model_path)
-top = model.recommendProducts(61,30)
-print top
